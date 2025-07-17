d6e7e59d0259c692b2d3d18a3f3ccb66cfdaf556
alaaddin.aydin <alaaddin.aydin@ordulu.com>
2025-07-17 17:28:52 +0300

    permission setting

--- CookBook/app/src/main/java/com/aydin/cookbook/CookFragment.kt
+++ CookBook/app/src/main/java/com/aydin/cookbook/CookFragment.kt
@@ -1,17 +1,40 @@
 package com.aydin.cookbook
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.aydin.cookbook.databinding.FragmentCookBinding
+import com.google.android.material.snackbar.Snackbar
 
 class CookFragment : Fragment() {
     private var _binding: FragmentCookBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+
+    private var selectedImagesUri : Uri? = null
+    private var selecteImagesBitmap : Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        registerLauncher()
     }
 
     override fun onCreateView(
@@ -52,7 +75,94 @@ class CookFragment : Fragment() {
 
     }
     fun selectImage(view: View){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+                //İzin Verilmedi
+                if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.READ_MEDIA_IMAGES)){
+                    //snackbar göster, kullanıcıdan nedne izin istediğini ona belirt.
+                    Snackbar.make(view, "Galeriye ulamamız lazım.", Snackbar.LENGTH_INDEFINITE).setAction(
+                        "İzin Ver",
+                        View.OnClickListener {
+                            //izin istenecek
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                        }
+                    )
+                } else {
+                    // izin istenecek
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                }
+            } else {
+                // İzin Veridi galeriye git.
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                //İzin Verilmedi
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                ) {
+                    //snackbar göster, kullanıcıdan nedne izin istediğini ona belirt.
+                    Snackbar.make(view, "Galeriye ulamamız lazım.", Snackbar.LENGTH_INDEFINITE)
+                        .setAction(
+                            "İzin Ver",
+                            View.OnClickListener {
+                                //izin istenecek
+                                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            }
+                        )
+                } else {
+                    // izin istenecek
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            } else {
+                // İzin Veridi galeriye git.
+                val intentToGallery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }
+        }
+    }
 
+    private fun registerLauncher() {
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
+            if(result.resultCode == AppCompatActivity.RESULT_OK){
+                val intentfromResult = result.data
+                if (intentfromResult != null){
+                    selectedImagesUri = intentfromResult.data
+                    // URI to Bitmap
+                    try{
+                        if (Build.VERSION.SDK_INT >= 28) {
+                            val source = ImageDecoder.createSource(requireActivity().contentResolver,selectedImagesUri!!)
+                            selecteImagesBitmap = ImageDecoder.decodeBitmap(source)
+                            binding.imageView.setImageBitmap(selecteImagesBitmap)
+                        } else {
+                            selecteImagesBitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver,selectedImagesUri)
+                            binding.imageView.setImageBitmap(selecteImagesBitmap)
+                        }
+                    }catch (e : Exception){
+                        println(e.localizedMessage)
+                    }
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if(result){
+                // izin verildi galeriye gidilebilir.
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            } else {
+                // İzin verilmedi yapacak bir şey yok
+                Toast.makeText(requireContext(),"İzin vermediniz", Toast.LENGTH_LONG)
+            }
+        }
     }
     override fun onDestroyView() {
         super.onDestroyView()
